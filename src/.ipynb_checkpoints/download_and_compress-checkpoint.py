e9330673fc59825a1a520b98866de10f21586304
BrofessorDoucette <brofessordoucette@gmail.com>
2025-03-08 19:55:36 -0600

    Updated Data Preprocessing Code, interpolated RBSP data

---
+++ src/.ipynb_checkpoints/download_and_compress-checkpoint.py
@@ -0,0 +1,40 @@
+import data_compressor
+import data_scraper
+
+
+def download_and_compress_year_rept_l2(satellite: str, year: int) -> None:
+    
+    for month in range(1, 13):
+        
+        data_scraper.download_month_rbsp_rept_l2(satellite = satellite.upper(),
+                                            month = month, year = year,
+                                            make_dirs = True)
+
+        data_compressor.compress_month_rept_l2(satellite = satellite.upper(),
+                                               month = month, year = year,
+                                               make_dirs = True)
+
+
+def download_and_compress_month_rept_l2(satellite: str, month: int, year: int) -> None:
+    
+    data_scraper.download_month_rbsp_rept_l2(satellite = satellite.upper(),
+                                        month = month, year = year,
+                                        make_dirs = True)
+
+    data_compressor.compress_month_rept_l2(satellite = satellite.upper(),
+                                           month = month, year = year,
+                                           make_dirs = True)
+
+
+def download_and_compress_poes_metop(satellite: str) -> None:
+ 
+    data_scraper.download_year_poes_after_2012(satid=satellite.lower(),
+                                          make_dirs=True)
+
+    data_compressor.compress_poes_after_2012(satellite=satellite.lower(),
+                                  make_dirs=True)
+
+
+if __name__ == '__main__':
+
+    download_and_compress_poes_metop("noaa19")
