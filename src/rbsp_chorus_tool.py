641800ea40cf9749d56eca0adcce1281380976b0
BrofessorDoucette <brofessordoucette@gmail.com>
2025-06-01 20:03:44 -0500

    Changes

--- src/rbsp_chorus_tool.py
+++ src/rbsp_chorus_tool.py
@@ -6,109 +6,95 @@ def emfisis_noise_floor(f):
     x = [1, 10, 100, 1000, 10000]
     y = [1e-4, 1e-5, 1e-8, 1e-10, 1e-10]
 
-    return np.exp(np.interp(np.log(f), np.log(x), np.log(y), left=np.nan, right=np.nan))
+    return np.exp(np.interp(np.log(f), np.log(x), np.log(y)))
+
 
 def emfisis_planarity_floor(f):
 
     x = [1, 10, 30, 100, 300, 1000, 3000, 10000]
     y = [1.0, 0.95, 0.90, 0.8, 0.6, 0.40, 0.25, 0.25]
 
-    return np.interp(np.log(f), np.log(x), y, left=np.nan, right=np.nan)
+    return np.interp(np.log(f), np.log(x), y)
 
 
-def calculate_chorus_amplitudes_from_bsum(B_uvw, B_sum, WFR_bandwidths, WFR_frequencies) -> tuple:
+def calculate_cyclotron_frequency(B):
 
     m_e = 9.10938356e-31  # Mass of electron in kg
     q = 1.60217662e-19  # Charge of electron
 
+    w_ce = (q * np.asarray(B)) / (m_e)
+    f_ce = w_ce / (2 * np.pi)  # Cyclotron frequency in Hz
+
+    return f_ce
+
+
+def calculate_chorus_amplitudes_from_bsum(B_uvw, B_sum, Magnetic_Planarity, Magnetic_Ellipticity, lower=True) -> tuple:
+
     B = np.sqrt(B_uvw[:, 0] ** 2 + B_uvw[:, 1] ** 2 + B_uvw[:, 2] ** 2)
     B = B / (1e9)  # Magnetic field magnitude in Tesla
 
-    w_ce = (q * B) / (m_e)
-    f_ce = w_ce / (2 * np.pi)  # Cyclotron frequency in Hz
+    f_ce = calculate_cyclotron_frequency(B)
 
-    min_f_for_lower_chorus_band = 5 * (f_ce / 100.0)
-    max_f_for_lower_chorus_band = 5 * (f_ce / 10.0)
+    if lower:
+        min_f = 5 * (f_ce / 100.0)
+        max_f = 5 * (f_ce / 10.0)
+    else:
+        min_f = 5 * (f_ce / 10.0)
+        max_f = f_ce
 
-    min_f_for_upper_chorus_band = 5 * (f_ce / 10.0)
-    max_f_for_upper_chorus_band = f_ce
+    WNA_frequencies_bandwidths = pd.read_csv("./../EMFISIS_WNA_frequencies_and_bandwidths.csv")
+    WNA_frequencies = WNA_frequencies_bandwidths["f_l"]
+    WNA_bandwidths = WNA_frequencies_bandwidths["del_l"]
 
-    frequency_bin_minimums = WFR_frequencies - (WFR_bandwidths / 2)
-    frequency_bin_maximums = WFR_frequencies + (WFR_bandwidths / 2)
+    print(WNA_frequencies)
+    print(WNA_bandwidths)
 
-    noise_floor = emfisis_noise_floor(WFR_frequencies)
+    frequency_bin_minimums = WNA_frequencies - (WNA_bandwidths / 2)
+    frequency_bin_maximums = WNA_frequencies + (WNA_bandwidths / 2)
 
-    lower_band_chorus = []
-    upper_band_chorus = []
+    noise_floor = emfisis_noise_floor(WNA_frequencies)
+    planarity_floor = 0.6
+    ellipticity_floor = 0.7
+
+    chorus = []
 
     for T in range(B_uvw.shape[0]):
 
-        lower_band_frequency_bins = (min_f_for_lower_chorus_band[T] <= frequency_bin_minimums) & (frequency_bin_maximums < max_f_for_lower_chorus_band[T])
-        upper_band_frequency_bins = (min_f_for_upper_chorus_band[T] <= frequency_bin_minimums) & (frequency_bin_maximums < max_f_for_upper_chorus_band[T])
+        frequency_bins = (min_f[T] <= frequency_bin_minimums) & (frequency_bin_maximums < max_f[T])
 
-        if np.any(lower_band_frequency_bins):
+        if np.any(frequency_bins):
 
-            bandwidths = WFR_bandwidths[lower_band_frequency_bins]
-            power_hz = B_sum[T, lower_band_frequency_bins]
+            bandwidths = WNA_bandwidths[frequency_bins]
+            power_hz = B_sum[T, frequency_bins].flatten()
+            planarity = Magnetic_Planarity[T, frequency_bins].flatten()
+            ellipticity = Magnetic_Ellipticity[T, frequency_bins].flatten()
 
-            greater_than_noise_floor = (power_hz > 10 * noise_floor[lower_band_frequency_bins])  # Should be an order of magnitude greater than noise floor
+            greater_than_noise_floor = (power_hz > 10 * noise_floor[frequency_bins])  # Should be an order of magnitude greater than noise floor
+            greater_than_planarity_floor = (planarity >= planarity_floor)
+            greater_than_ellipticity_floor = (ellipticity >= ellipticity_floor)
 
-            if np.any(greater_than_noise_floor):
+            satisfies_filters = greater_than_noise_floor & greater_than_planarity_floor & greater_than_ellipticity_floor
 
-                power = np.nansum(power_hz[greater_than_noise_floor] * bandwidths[greater_than_noise_floor])
-                amplitude = np.sqrt(power)
-                lower_band_chorus.append(amplitude * 1000.0)  # Convert from nT to pT
+            if np.any(satisfies_filters):
+
+                power = np.nansum(power_hz[satisfies_filters] * bandwidths[satisfies_filters])
+                chorus.append((np.sqrt(power) * 1000.0)**2)  # Convert from nT to pT
             else:
-                lower_band_chorus.append(0)
+                chorus.append(0.01)
         else:
-            lower_band_chorus.append(np.nan)
+            chorus.append(np.nan)
 
-        if np.any(upper_band_frequency_bins):
-
-            bandwidths = WFR_bandwidths[upper_band_frequency_bins]
-            power_hz = B_sum[T, upper_band_frequency_bins]
-
-            greater_than_noise_floor = (power_hz > 10 * noise_floor[upper_band_frequency_bins])  # Should be an order of magnitude greater than noise floor
-
-            if np.any(greater_than_noise_floor):
-
-                power = np.nansum(power_hz[greater_than_noise_floor] * bandwidths[greater_than_noise_floor])
-                amplitude = np.sqrt(power)
-                upper_band_chorus.append(amplitude * 1000.0)  # Convert from nT to pT
-            else:
-                upper_band_chorus.append(0)
-        else:
-            upper_band_chorus.append(np.nan)
-
-    return lower_band_chorus, upper_band_chorus
+    return chorus
 
 
-def iterate_through_days_and_calculate_chorus_amplitudes(WNA_survey, WFR_spectral_matrix) -> dict:
+def calculate_chorus_power(WNA_survey, Magnetic_Planarity, Magnetic_Ellipticity, lower=True) -> dict:
 
-    lower_band_chorus = []
-    upper_band_chorus = []
+    chorus_power = calculate_chorus_amplitudes_from_bsum(
+        B_uvw=WNA_survey["Buvw"],
+        B_sum=WNA_survey["bsum"],
+        Magnetic_Planarity=Magnetic_Planarity,
+        Magnetic_Ellipticity=Magnetic_Ellipticity,
+        lower=lower
+    )
 
-    # This makes sure that the same number of days were loaded
-    assert len(WNA_survey["timestamps_per_file"]) == WFR_spectral_matrix["WFR_bandwidth"].shape[0]
-
-    index = 0
-    for day in range(len(WNA_survey["timestamps_per_file"])):
-
-        num_timesteps_for_day = WNA_survey["timestamps_per_file"][day]
-
-        chorus_calculated = calculate_chorus_amplitudes_from_bsum(
-            WNA_survey["Buvw"][index : index + num_timesteps_for_day],
-            WNA_survey["bsum"][index : index + num_timesteps_for_day],
-            WFR_spectral_matrix["WFR_bandwidth"][day, :],
-            WFR_spectral_matrix["WFR_frequencies"][day, :],
-        )
-
-        lower_band_chorus.extend(chorus_calculated[0])
-        upper_band_chorus.extend(chorus_calculated[1])
-
-        index = index + num_timesteps_for_day
-
-    return {
-        "Lower_Band": np.asarray(lower_band_chorus),
-        "Upper_Band": np.asarray(upper_band_chorus),
-    }
+    return chorus_power
